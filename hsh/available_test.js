40a249f33aa527451ffe4dd77e08026f16cf32ce
bunchsh <onee.ssong@gmail.com>
2021-12-06 16:05:11 +0900

    hsh test

--- hsh/available_test.js
+++ hsh/available_test.js
@@ -18,11 +18,11 @@ document.querySelector('#join_form').addEventListener("submit", e => {
     if (!regexHelper.max_length('#id', 20, '아이디는 최대 20자까지만 입력 가능합니다.')) { return false; }
     if (!regexHelper.eng_num('#id', '아이디는 영어와 숫자 조합만 입력 가능합니다.')) { return false; }
 
-     /** 비밀번호 검사 */
-     if (!regexHelper.value("#pw", "비밀번호를 입력하세요.")) {return false;}
-     if (!regexHelper.min_length("#pw", 8, "비밀번호는 최소 8자 이상 입력 가능합니다.")) {return false;}
-     if (!regexHelper.max_length("#pw", 16, "비밀번호는 최대 16자까지 입력 가능합니다.")) {return false;}
-     if (!regexHelper.compare_to("#pw", "#pw_re", "비밀번호 확인이 잘못되었습니다")) {return false;}
+    /** 비밀번호 검사 */
+    if (!regexHelper.value("#pw", "비밀번호를 입력하세요.")) {return false;}
+    if (!regexHelper.min_length("#pw", 8, "비밀번호는 최소 8자 이상 입력 가능합니다.")) {return false;}
+    if (!regexHelper.max_length("#pw", 16, "비밀번호는 최대 16자까지 입력 가능합니다.")) {return false;}
+    if (!regexHelper.compare_to("#pw", "#pw_re", "비밀번호 확인이 잘못되었습니다")) {return false;}
 
      /** 이름 검사 */
     if (!regexHelper.value('#name', '이름을 입력하세요.')) { return false; }
@@ -37,6 +37,11 @@ document.querySelector('#join_form').addEventListener("submit", e => {
     // 연락처 검사
     if(!regexHelper.value('#tel', '연락처를 입력하세요.')) {return false;}
     if(!regexHelper.cellphone('#tel', '연락처가 잘못되었습니다.')) {return false;}
+
+    // 주소 검사
+    if(!regexHelper.value('#postcode', '주소를 입력하세요.')) {return false;}
+    if(!regexHelper.value('#address', '주소를 입력하세요.')) {return false;}
+    if(!regexHelper.value('#address_detail', '상세주소를 입력하세요.')) {return false;}
 
      /** 처리 완료 */
      alert("가입이 완료되었습니다.");
