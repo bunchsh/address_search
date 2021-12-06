dc7c2a0a97a391cba735f9d2e62f5dc50bc353a4
bunchsh <onee.ssong@gmail.com>
2021-12-06 14:27:53 +0900

    비밀번호 유효성

---
+++ js/password.js
@@ -0,0 +1,17 @@
+document.write("<script src='/js/regex_helper.js'></script>");
+
+/**--- 유효성 검사 ---*/
+document.querySelector('#join_form').addEventListener("submit", e => {
+    e.preventDefault();
+
+    const regexHelper = new RegexHelper();
+
+     /** 비밀번호 검사 */
+     if (!regexHelper.value("#pw", "비밀번호를 입력하세요.")) {return false;}
+     if (!regexHelper.min_length("#pw", 8, "비밀번호는 최소 8자 이상 입력 가능합니다.")) {return false;}
+     if (!regexHelper.max_length("#pw", 16, "비밀번호는 최대 16자까지 입력 가능합니다.")) {return false;}
+     if (!regexHelper.compare_to("#pw", "#pw_re", "비밀번호 확인이 잘못되었습니다")) {return false;}
+
+     /** 처리 완료 */
+     alert("가입이 완료되었습니다.");
+});
